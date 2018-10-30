1eec5ef239316f6b60c9c78c265a52239f69154a
Chase Whitney <ChaseEWhitney@gmail.com>
2018-10-29 21:03:49 -0500

    changed route handling to work with react-router-config

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
 
 import Home from './components/Home';
 import UsersList from './components/UsersList';
 
-export default () => {
-  return (
-    <div>
-      <Route exact path='/' component={Home} />
-      <Route path='/users' component={UsersList} />
-    </div>
-  );
-};
+export default [
+  {
+    path: '/',
+    exact: true,
+    component: Home
+  },
+  {
+    path:'/users',
+    component: UsersList
+  }
+];
+
+// Use different syntax for react-router-config
+// export default () => {
+//   return (
+//     <div>
+//       <Route exact path='/' component={Home} />
+//       <Route path='/users' component={UsersList} />
+//     </div>
+//   );
+// };
--- src/client/reducers/usersReducer.js
+++ src/client/reducers/usersReducer.js
@@ -3,7 +3,6 @@ import { FETCH_USERS } from '../actions';
 export default (state = [], action) => {
   switch(action.type) {
     case FETCH_USERS:
-      console.log('payload is:', action.payload);
       return action.payload.data;
     default:
       return state;
